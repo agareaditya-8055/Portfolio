f240b906e997ca33d88310b653f215c30f9770c5
agareaditya-8055 <adityaagare8055@gmail.com>
2025-04-03 23:03:28 +0530

    Resume added

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -61,7 +61,7 @@ const HeroSection: React.FC = () => {
               </Link>
             </Button>
             <Button variant="outline" size="lg" asChild>
-              <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              <a href="/aditya_resume.pdf" target="_blank" rel="noopener noreferrer">
                 <FileText className="mr-2 h-4 w-4" /> Resume
               </a>
             </Button>
